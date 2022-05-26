87e5df3b1e5a8239ec16d1f643b7ff74d436c540
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2022-05-26 19:02:14 +0200

    Add axios functionality

---
+++ src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-row class="text-left">
+      <ul>
+        <li v-for="(todo, id) in todos" :key="todo.id"> {{ id }}, {{ todo.title }} </li>
+      </ul>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+const axios = require('axios').default;  
+
+// JSONPlaceHolder URLs
+const url_users = "https://jsonplaceholder.typicode.com/users";
+const url_todos = "https://jsonplaceholder.typicode.com/todos";
+
+export default {
+  name: 'HomePage',
+
+  data() {
+    return {
+      // Create arrays for resources
+      users: [],
+      todos: []
+    }
+  },
+  methods: {
+    
+  },
+  created() {
+    // GET request for JSONPlaceHolder /users
+    axios.get(url_users)
+      .then(response => {
+        // Write user data into the users array
+        this.users = response.data;
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    // GET request for JSONPlaceHolder /todos
+    axios.get(url_todos)
+      .then(response => {
+        // Write todo list data into the todos array
+        this.todos = response.data;
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
+</script>
---
+++ src/views/HomePageView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <home-page/>
+  </div>
+</template>
+
+<script>
+import HomePage from '@/components/HomePage.vue';
+
+export default {
+  name: 'HomePageView',
+
+  components: {
+    HomePage
+  }
+}
+</script>
