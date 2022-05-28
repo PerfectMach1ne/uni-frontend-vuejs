9b975fd2173956a21256775bb8d92ef4c8349829
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2022-05-28 14:52:02 +0200

    Add a navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <v-app>
     <v-main>
+      <v-app-bar
+        color="green"
+        flat
+      >
+        <v-item>
+          <router-link to="/" class="router-link">Home</router-link>
+        </v-item>
+      </v-app-bar>
       <router-view/>
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+.router-link {
+  font-size: 20px;
+  color: azure;
+}
+</style>
 
 <script>
 
 export default {
   name: 'App',
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+
+    }
+  }
 };
 </script>
